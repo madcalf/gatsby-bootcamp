8371376873c81faed876289b4e020e74225c863b
D Dixon <d@themadcalf.com>
2021-06-09 11:53:31 -0700

    Add some basic styling to footer and blog list page.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../components/layout';
 import { graphql, useStaticQuery, Link } from 'gatsby';
+import * as blogStyles from './blog.module.scss';
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
@@ -25,15 +26,14 @@ const BlogPage = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>Posts will go here later</p>
-      <ol>
+      <ol className={blogStyles.posts}>
         {posts.map(post => {
           return (
-            <li key={post.node.id}>
+            <li key={post.node.id} className={blogStyles.post}>
               <Link to={`../blog/${post.node.fields.slug}`}>
                 <h1>{post.node.frontmatter.title}</h1>
+                <p>{post.node.frontmatter.date}</p>
               </Link>
-              <p>{post.node.frontmatter.date}</p>
             </li>
           );
         })}
