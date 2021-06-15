8155cef51558a6edd4b8365588c15545a0ea6bab
D Dixon <d@themadcalf.com>
2021-06-15 15:05:33 -0700

    Add custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+
+const NotFound = () => {
+  return (
+    <Layout>
+      <h1>Page not found</h1>
+      <p>
+        <Link to="/">Head for home</Link>
+      </p>
+    </Layout>
+  );
+};
+
+export default NotFound;
